9c8b4a65ee733bbe3735d799d46537681f26eeb5
Nateusz <mateusz@mail.com>
2016-01-06 01:11:56 +0100

    Add source code

---
+++ src/Dummy.java
@@ -0,0 +1,7 @@
+package dummy;
+
+public class Dummy {
+	public String toString() {
+		return "some string:"+1;
+	}
+}
---
+++ src/class1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class class1 {
+	public static final String EXAMPLE_NAME = "name";
+	
+	public class1() {
+		
+	}
+	
+	public String getName() {
+		return class1.EXAMPLE_NAME;
+	}
+}
